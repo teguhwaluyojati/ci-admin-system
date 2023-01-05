1149c9add48f2f098ca4d589a6db90ac6f0beae8
teguhwaluyojati <teguhwaluyojati14@gmail.com>
2023-01-05 10:31:27 +0700

    add new controller

---
+++ application/controllers/twjshop/User.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+
+    public function index()
+    {
+        $data['title'] = "TWJ Shop";
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+        
+        $this->load->view('twjshop/index', $data);
+    }
+}
